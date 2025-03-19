e2bbae36215753d46a43781e94703cf10f920817
Taras Basiuk <BasiukTV@gmail.com>
2025-03-19 15:49:02 -0400

    Adding a small Hello World Java app.

---
+++ dir/sample_file.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Taras was here!");
+    }
+}
